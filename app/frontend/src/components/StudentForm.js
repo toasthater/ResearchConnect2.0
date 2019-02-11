74dcc4edcd6b5932c98ee8d6f72c55db561a7535
Giulio Colleluori <gcollelu@ucsc.edu>
2019-02-10 20:47:29 -0800

    now creating student entry when user first signs up

---
+++ app/frontend/src/components/StudentForm.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
+class StudentForm extends Component {
+
+
+    render() {
+        const name = this.props.name;
+        
+        return (
+            <form onSubmit={this.handleSubmit} >
+            
+            <br/>
+
+            <div className="columns">
+                <div className="column is-6">
+                <div className="field">
+                    <label className="label">Display Name</label>
+                    <div className="control">
+                    <input className="input" type="text" value={name} required onChange={this.handleChange} />
+                    </div>
+                </div>
+
+                <div className="field">
+                    <label className="label">Major</label>
+                    <div className="control">
+                    <input className="input" type="text" defaultValue="N/A" />
+                    </div>
+                </div>            
+                </div>
+                <div className="column is-5 is-offset-1">
+                <div className="field">
+                    <label className="label">Profile Picture</label>
+                    <figure className="image shadowed" style={{ height: 256, width: 256, background: 'white' }}>
+                    </figure>
+                    <br/>
+                    <button className="button">Select Image </button>
+                </div>
+                </div>
+            </div>
+            <div className="field">
+                <div className="control">
+                <button type="submit" className="button is-success">Save</button>
+                </div>
+            </div>
+            </form>
+        )
+    }
+}
+
+function mapStateToProps({auth}){
+    return { auth };
+  }
+
+export default connect(mapStateToProps)(StudentForm);
--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -18,21 +18,6 @@ module.exports = app => {
 
     app.get('/api/current_user', async (req, res) => {
         res.send(req.user);
-        // if (req.user.isProfessor){
-        //     let relevantFaculty = FacultyMember.find({
-        //         'cruzid': {
-        //             '$regex': req.user.cruzid,
-        //             $options: 'i'
-        //         }
-        //     });
-        //     relevantFaculty.then(async (facultyMember) => {
-        //         console.log(facultyMember);
-        //         res.send(req.user);
-        //     });
-        // }
-        // else{
-        //     res.send(req.user);
-        // }
         
         });
 
