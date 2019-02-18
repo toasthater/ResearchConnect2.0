77fb0ed05a4fec3155175312c8054465778be5aa
har95337 <hsingh18@ucsc.edu>
2019-02-18 12:23:46 -0800

    Viewing relevant post details started

--- app/frontend/src/store.js
+++ app/frontend/src/store.js
@@ -2,7 +2,7 @@ import { createStore , applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
 import reduxThunk from 'redux-thunk';
 
-const initialState = {loadState: 0, auth: false};
+const initialState = {loadState: 0, auth: false, search: []};
 const middleware = [reduxThunk];
 const store = createStore(reducers, initialState, compose(
     applyMiddleware(...middleware),
