ed77d59c242b0eebd4b836e7116cf219dce9abee
Gene Choi <gkchoi@ucsc.edu>
2019-02-18 00:21:11 -0800

    adding profile component

--- app/routes/api/students.js
+++ app/routes/api/students.js
@@ -1,24 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Student = require('../../models/Student');
+const Student = require("../../models/Student");
 
-router.get('/', async (req, res) => {
-    let relevantStudents = Student.find({
-            'cruzid': {
-                '$regex': req.query.cruzid,
-                $options: 'i'
-            }
-        });
-    await relevantStudents.then(async (student) => {
-        res.send(student);
-    });
+//router.get("/:cruzid", async (req, res) =>
+router.get("/", async (req, res) => {
+  Student.findOne({ cruzid: req.query.cruzid })
+    .then(students => res.send(students))
+    .catch(console.log);
 });
 
-router.delete('/:id', (req, res) => {
-    Student.findById(req.params.id)
-        .then(research => research.remove().then(() => res.json({success: true})))
-        .catch(err => res.status(404).json({success: true}));
+router.post("/", async (req, res) => {
+  const profile = new Student({
+    cruzid: req.body.cruzid,
+    name: req.body.name,
+    email: req.body.email,
+    major: req.body.major,
+    bio: req.body.bio,
+    resume: req.body.resume
+  });
+
+  profile.save().then(students => res.json(students));
+});
+
+router.delete("/", (req, res) => {
+  Student.deleteOne({ cruzid: req.query.cruzid })
+    .then(students => res.send(students))
+    .catch(console.log);
 });
 
 module.exports = router;
--- app/server.js
+++ app/server.js
@@ -1,29 +1,32 @@
-const express = require('express');
-const keys = require('./config/keys');
-const passport = require('passport');
-const cookieSession = require('cookie-session');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const researchPosts = require('./routes/api/research_posts');
-const department = require('./routes/api/department');
-const facultyMember = require('./routes/api/faculty_members');
-const search = require('./routes/api/search');
-const authRoutes = require('./routes/authRoutes');
-const users = require('./routes/api/users');
-require('./models/User');
-require('./models/Student');
-require('./models/FacultyMember');
-require('./models/Department');
-require('./models/Research');
-require('./models/Application');
-require('./services/passport');
+const express = require("express");
+const keys = require("./config/keys");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const researchPosts = require("./routes/api/research_posts");
+const department = require("./routes/api/department");
+const facultyMember = require("./routes/api/faculty_members");
+const studentMember = require("./routes/api/students");
+const search = require("./routes/api/search");
+const authRoutes = require("./routes/authRoutes");
+const users = require("./routes/api/users");
+require("./models/User");
+require("./models/Student");
+require("./models/FacultyMember");
+require("./models/Department");
+require("./models/Research");
+require("./models/Application");
+require("./services/passport");
 
 const app = express();
 
-app.use(cookieSession({
-    maxAge: 30*24*60*60*1000,
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
-}));
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -34,21 +37,21 @@ authRoutes(app);
 app.use(bodyParser.json());
 
 // Use routes
-app.use('/api/search', search);
-app.use('/api/research_posts', researchPosts);
-app.use('/api/department', department);
-app.use('/api/faculty_members', facultyMember);
-app.use('/api/user', users);
+app.use("/api/search", search);
+app.use("/api/research_posts", researchPosts);
+app.use("/api/department", department);
+app.use("/api/faculty_members", facultyMember);
+app.use("/api/user", users);
+app.use("/api/students", studentMember);
 
 // DB config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // Connect to Mongo
 mongoose
-    .connect(db)
-    .then(() => console.log("MongoDB connected...."))
-    .catch(err => console.log(err));
-
+  .connect(db)
+  .then(() => console.log("MongoDB connected...."))
+  .catch(err => console.log(err));
 
 const port = process.env.port || 5000;
 
