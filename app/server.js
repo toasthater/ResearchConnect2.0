77eb6b69b2446846fe97756c1aae6864d6505231
Manthan Mallikarjun <nahtnam@gmail.com>
2019-04-13 17:46:01 -0700

    auto lint master

--- app/server.js
+++ app/server.js
@@ -72,7 +72,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'frontend/build')));
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
   });
 }
