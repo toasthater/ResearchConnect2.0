d52ee27d9ba3d1caddcbda52916bf632e66498e4
gkchoi <43891129+HybridXSV@users.noreply.github.com>
2019-04-21 13:46:27 -0700

    Update store.js

--- app/frontend/src/store.js
+++ app/frontend/src/store.js
@@ -8,5 +8,5 @@ const store = createStore(reducers, initialState, compose(
     applyMiddleware(...middleware)
 ));
 
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 export default store;
