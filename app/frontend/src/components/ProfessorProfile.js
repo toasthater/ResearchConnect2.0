63d8b57a60ab4093d912f04a07c48d70bd1eced2
Manthan Mallikarjun <nahtnam@gmail.com>
2019-04-28 17:24:12 -0700

    lint frontend

--- app/frontend/src/components/ProfessorProfile.js
+++ app/frontend/src/components/ProfessorProfile.js
@@ -1,61 +1,59 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import profileImg from "../assets/profile.png";
+import axios from 'axios';
+import profileImg from '../assets/profile.png';
 import Spinner from './Spinner';
-import axios from "axios";
 
 class ProfessorProfile extends Component {
-
   componentDidMount() {
-    this.checkFollowers()
+    this.checkFollowers();
   }
 
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       loading: true,
-      isFollowDisabled: this.props.isFollowDisabled
-    }
+      isFollowDisabled: this.props.isFollowDisabled,
+    };
   }
 
   checkFollowers = () => {
     axios
-      .get("/api/users/", {
+      .get('/api/users/', {
         params: {
-          cruzid: this.props.auth.cruzid
-        }
+          cruzid: this.props.auth.cruzid,
+        },
       })
-      .then(res => {
+      .then((res) => {
         if (res.data.following && res.data.following.includes(this.props.match.params.cruzid)) {
           this.setState({
             following: true,
-            loading: false
-          })
-        }
-        else {
+            loading: false,
+          });
+        } else {
           this.setState({
             following: false,
-            loading: false
-          })
+            loading: false,
+          });
         }
-      })
+      });
   }
 
-  toggleFollow = _ => {
+  toggleFollow = (_) => {
     const following = this.state.following;
     this.setState({ isFollowDisabled: false });
     this.setState({ following: !this.state.following });
 
     axios
-      .post("/api/follow", {
+      .post('/api/follow', {
         cruzid: this.props.match.params.cruzid,
-        following: following
+        following,
       })
       .then(_ => this.setState({ isFollowDisabled: false }))
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
-        this.setState({ following: following });
+        this.setState({ following });
       });
   };
 
@@ -74,7 +72,8 @@ class ProfessorProfile extends Component {
             <br />
             <div align="center">
               <figure className="image is-128x128">
-                <img className="is-rounded"
+                <img
+                  className="is-rounded"
                   src={
                     this.props.profile.profile_pic
                       ? this.props.profile.profile_pic
@@ -90,40 +89,41 @@ class ProfessorProfile extends Component {
           {!myProfile && (
             <div>
               <button
-                className={"button is-link " + (this.state.following ? "" : "is-inverted")}
+                className={`button is-link ${ this.state.following ? '' : 'is-inverted'}`}
                 disabled={this.state.isFollowDisabled}
                 onClick={this.toggleFollow}
               >
-                {this.state.following ? "Following" : "Follow"}
+                {this.state.following ? 'Following' : 'Follow'}
               </button>
-              <br /><br />
+              <br />
+              <br />
             </div>
           )}
 
           <div className="box">
-            {this.props.profile.name ? this.props.profile.name : "No Name Listed"}
+            {this.props.profile.name ? this.props.profile.name : 'No Name Listed'}
           </div>
 
           <div className="box">
-            {this.props.professor.title ? this.props.professor.title : "No Title Listed"}
+            {this.props.professor.title ? this.props.professor.title : 'No Title Listed'}
           </div>
 
           <div className="box">
-            {this.props.profile.email ? this.props.profile.email : "No Email Listed"}
+            {this.props.profile.email ? this.props.profile.email : 'No Email Listed'}
           </div>
 
           {<div className="box">
-            {this.props.professor.department ? this.props.professor.department : "No Department Listed"}
+            {this.props.professor.department ? this.props.professor.department : 'No Department Listed'}
           </div>}
 
           <div className="box">
             <p align="left">Bio:</p>
-            <p align="left">{this.props.profile.bio ? this.props.profile.bio : "No Available Bio"}</p>
+            <p align="left">{this.props.profile.bio ? this.props.profile.bio : 'No Available Bio'}</p>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default withRouter(ProfessorProfile)
+export default withRouter(ProfessorProfile);
